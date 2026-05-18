9a35a6bddfcfa63527c61ab3b51ba8c5b4ea640b
César <06cesar.leon@gmail.com>
2026-05-18 02:10:14 -0600

    cambio en DTOs ApiResponse pasa a generico, OrderResponse ignora campos no pedidos y cambios en CartService mejorando verificaciones y mandando los campos correctos requeridos para la integracion

--- src/main/java/com/project/customer_experience/dto/response/OrderResponseDTO.java
+++ src/main/java/com/project/customer_experience/dto/response/OrderResponseDTO.java
@@ -1,7 +1,10 @@
 package com.project.customer_experience.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.math.BigDecimal;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record OrderResponseDTO(
         Long id,
         String clientEmail,
